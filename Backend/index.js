161ee8e1b9d19af497864bd6d029ba8f572b1807
Andrea Núñez <nm0753032023@unab.edu.sv>
2026-04-06 02:10:58 -0600

    Implementación de base de datos PostgreSQL, creacioón de tablas producto y categoria y conexión con backend mediante

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Backend funcionando 🚀');
-});
+app.use(cors());
+app.use(express.json());
+
 
 app.listen(3000, () => {
   console.log('Servidor en puerto 3000');
--- Database/schema.sql
+++ Database/schema.sql
@@ -1,5 +1,15 @@
-CREATE TABLE productos (
-  id INT PRIMARY KEY,
-  nombre VARCHAR(100),
-  precio DECIMAL(10,2)
+CREATE TABLE categoria (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE producto (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  descripcion TEXT,
+  precio DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL,
+  categoria_id INT,
+  imagen TEXT,
+  FOREIGN KEY (categoria_id) REFERENCES categoria(id)
 );
