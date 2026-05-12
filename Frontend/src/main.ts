a929022896896b144dadd44c962b2a2a8fc16360
Andrea Núñez <nm0753032023@unab.edu.sv>
2026-05-12 01:44:28 -0600

    fix: agregar JavaScript de Bootstrap para menú hamburguesa

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
-// Aquí le decimos a Angular que arranque usando el AppComponent correcto
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
